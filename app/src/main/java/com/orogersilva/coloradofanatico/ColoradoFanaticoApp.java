2fe9806217efed10acb2d5674f26057069679159
Roger Silva <orogersilva@gmail.com>
2016-01-30 15:28:50 -0200

    Construindo primeiro módulo Dagger.

--- app/src/main/java/com/orogersilva/coloradofanatico/ColoradoFanaticoApp.java
+++ app/src/main/java/com/orogersilva/coloradofanatico/ColoradoFanaticoApp.java
@@ -3,6 +3,8 @@ package com.orogersilva.coloradofanatico;
 import android.app.Application;
 
 import com.orogersilva.coloradofanatico.di.component.AppComponent;
+import com.orogersilva.coloradofanatico.di.component.DaggerAppComponent;
+import com.orogersilva.coloradofanatico.di.module.DatabaseModule;
 
 /**
  * Created by orogersilva on 1/30/2016.
@@ -21,6 +23,10 @@ public class ColoradoFanaticoApp extends Application {
     public void onCreate() {
 
         super.onCreate();
+
+        mAppComponent = DaggerAppComponent.builder()
+                .databaseModule(new DatabaseModule(this))
+                .build();
     }
 
     // endregion
